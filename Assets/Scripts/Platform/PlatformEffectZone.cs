06ea14c7cbe80003b19828c8888327f85d5fe3c6
Shubham <shubham170905@gmail.com>
2026-08-05 16:58:00 +0530

    Added: Bouncy Platform Feedback

--- Assets/Scripts/Platform/PlatformEffectZone.cs
+++ Assets/Scripts/Platform/PlatformEffectZone.cs
@@ -1,3 +1,4 @@
+using MoreMountains.Feedbacks;
 using UnityEngine;
 
 namespace Valley.Platforms
@@ -6,7 +7,15 @@ namespace Valley.Platforms
     public class PlatformEffectZone : MonoBehaviour
     {
         [SerializeField] private PlatformEffectProfile profile;
+        [SerializeField] private MMF_Player collisionFeedback;
 
         public PlatformEffectProfile Profile => profile;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collisionFeedback != null) {
+                collisionFeedback.PlayFeedbacks();
+            }
+        }
     }
 }
