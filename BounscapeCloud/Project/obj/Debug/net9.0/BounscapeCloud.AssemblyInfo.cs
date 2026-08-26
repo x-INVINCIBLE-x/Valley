0439393b2faed1e781b7a966e1cb69dee3d95c7b
Shubham <shubham170905@gmail.com>
2026-08-26 22:49:36 +0530

    Added: Volume Switcher

--- BounscapeCloud/Project/obj/Debug/net9.0/BounscapeCloud.AssemblyInfo.cs
+++ BounscapeCloud/Project/obj/Debug/net9.0/BounscapeCloud.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BounscapeCloud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+508a2ab40bcb48a546c2827ba92c5371c5fda579")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8051889500b8048fa15a2cc7abadb3b3f8999f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BounscapeCloud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BounscapeCloud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
