5d79ef55b9687a1a2cfb2dd2b04ce4e81e50f512
Shubham <shubham170905@gmail.com>
2026-09-05 14:05:49 +0530

    Added: Launch UI

---
+++ Assets/UI/PlayerLaunchChargesUI.cs
@@ -0,0 +1,35 @@
+﻿using MoreMountains.Feedbacks;
+using TMPro;
+using UnityEngine;
+using Valley.Core;
+
+namespace Valley.UI
+{
+    public class PlayerLaunchChargesUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI chargesText;
+        [SerializeField] private PlayerLaunchGate launchGate;
+        [SerializeField] private MMF_Player feedback; 
+
+        private void OnEnable()
+        {
+            PlayerLaunchGate.OnChargesChanged += HandleChargesChanged;
+
+            if (launchGate != null)
+            {
+                chargesText.text = launchGate.Remaining.ToString();
+            }
+        }
+
+        private void OnDisable()
+        {
+            PlayerLaunchGate.OnChargesChanged -= HandleChargesChanged;
+        }
+
+        private void HandleChargesChanged(int remaining, int maxCharges)
+        {
+            chargesText.text = remaining.ToString();
+            feedback.PlayFeedbacks();
+        }
+    }
+}
